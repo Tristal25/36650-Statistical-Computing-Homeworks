6e80337d28e3d2133e17c93aa0c7e4871b69bb3d
Tristal Li <yanlinlily@126.com>
2021-10-22 23:20:48 -0400

    secure settings completed

--- HW-5/Q7/Q7.py
+++ HW-5/Q7/Q7.py
@@ -1,7 +1,9 @@
 import psycopg2
 
 def insert_employee():
-    conn = psycopg2.connect(database="postgres", user='postgres', password='Lylbigland25jly&', host='127.0.0.1', port= '5432')
+    secret = open('secret.txt', 'r')
+    pw = secret.read()[:-1]
+    conn = psycopg2.connect(database="postgres", user='postgres', password=pw, host='127.0.0.1', port='5432')
     cursor = conn.cursor()
     cursor.execute('''insert into employee (fname,lname)
     (select substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ',((random()*(26-1)+1)::integer),10), 
@@ -9,3 +11,6 @@ def insert_employee():
     from generate_series(1,500));''')
     conn.commit()
     conn.close()
+
+
+insert_employee()
